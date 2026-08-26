3073ce634fa59e37950d5fa6bcf17ac3b743413b
jagannathsangaria <jagannath.sangaria@gmail.com>
2026-08-26 23:55:47 +0530

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,11 +1,16 @@
 pluginManagement {
     val flutterSdkPath = run {
         val properties = java.util.Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
+        file("local.properties").inputStream().use {
+            properties.load(it)
+        }
+
         val flutterSdkPath = properties.getProperty("flutter.sdk")
+
         require(flutterSdkPath != null) {
             "flutter.sdk not set in local.properties"
         }
+
         flutterSdkPath
     }
 
@@ -20,8 +25,11 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+
     id("com.android.application") version "8.9.1" apply false
+
     id("org.jetbrains.kotlin.android") version "2.3.0" apply false
+
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
@@ -33,6 +41,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://storage.googleapis.com/download.flutter.io")
+        }
     }
 }
 
