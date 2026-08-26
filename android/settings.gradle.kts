59249b54b3012325c11ac1e1181a62f092bbdb28
jagannathsangaria <jagannath.sangaria@gmail.com>
2026-08-26 17:28:13 +0530

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -12,6 +12,7 @@ plugins {
     id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.3.0" apply false
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 dependencyResolutionManagement {
