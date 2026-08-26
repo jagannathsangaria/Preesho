24a7bba5b3a29d0924dc0408f70aa4219732c457
jagannathsangaria <jagannath.sangaria@gmail.com>
2026-08-26 17:15:03 +0530

    Create settings.gradle.kts

---
+++ android/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.9.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Preesho"
+include(":app")
