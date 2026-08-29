7a9828d959c12a7ed79b628a1983c6fc8e6a94e5
jagannathsangaria <jagannath.sangaria@gmail.com>
2026-08-29 22:45:44 +0530

    Add Flutter repository to settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -41,6 +41,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+
         maven {
             url = uri("https://storage.googleapis.com/download.flutter.io")
         }
