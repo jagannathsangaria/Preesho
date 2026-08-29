d94a7787db428377730f158ae631bad23cafaa60
jagannathsangaria <jagannath.sangaria@gmail.com>
2026-08-29 17:38:52 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,3 +4,11 @@ allprojects {
         mavenCentral()
     }
 }
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(
+            org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        )
+    }
+}
