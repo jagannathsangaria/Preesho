17a6d290c4d27525717a1a674bcbcc028372ae7c
jagannathsangaria <jagannath.sangaria@gmail.com>
2026-09-05 18:13:45 +0530

    Update checkout_page.dart

--- lib/checkout_page.dart
+++ lib/checkout_page.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
-import 'model/model.dart' show CustomerAddress;
 import 'models/preesho_models.dart';
 import 'orders_page.dart';
 import 'cart/cart_controller.dart';
