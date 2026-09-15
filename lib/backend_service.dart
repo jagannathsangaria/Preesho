58feebcce7e930a2a01622e9b38ac32671dedce8
jagannathsangaria <jagannath.sangaria@gmail.com>
2026-09-15 22:18:39 +0530

    Refactor saveAddress, createOrder, and orders methods

--- lib/backend_service.dart
+++ lib/backend_service.dart
@@ -1,3 +1,4 @@
+
 /// Production backend interface.
 /// Connect this interface to Firebase/Supabase/API after project credentials are supplied.
 abstract class BackendService {
@@ -24,14 +25,11 @@ class LocalBackendService implements BackendService {
   Future<List<Map<String, dynamic>>> products() async => [];
 
   @override
-  Future<void> saveAddress(Map<String, dynamic> address) async =>
-      _addresses.add(address);
+  Future<void> saveAddress(Map<String, dynamic> address) async => _addresses.add(address);
 
   @override
-  Future<void> createOrder(Map<String, dynamic> order) async =>
-      _orders.add(order);
+  Future<void> createOrder(Map<String, dynamic> order) async => _orders.add(order);
 
   @override
-  Future<List<Map<String, dynamic>>> orders() async =>
-      List.unmodifiable(_orders);
+  Future<List<Map<String, dynamic>>> orders() async => List.unmodifiable(_orders);
 }
